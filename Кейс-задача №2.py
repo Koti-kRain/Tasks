b7e2d795c9bddd281ea8eeb336d546397bf2f72d
Koti-kRain <vanI_14997@mail.ru>
2024-10-08 23:20:31 +0300

    Update Кейс-задача №2.py

--- Кейс-задача №2.py
+++ Кейс-задача №2.py
@@ -1,33 +1,42 @@
 import datetime
 import calendar
 
-def day_of_week(day,month,year):
+
+def get_birthday():
+    day = int(input("Введите день рождения: "))
+    month = int(input("Введите месяц рождения: "))
+    year = int(input("Введите год рождения: "))
+    return day, month, year
+
+def day_of_week(day, month, year):
     week_days = ['Понедельник','Вторник', 'Среда','Четверг','Пятница','Суббота','Воскресенье']
     target_date = datetime.datetime(year,month,day)
     day_idx = target_date.weekday()
     return week_days[day_idx]
 
 def is_leap_year(year):
-    return (year % 4 == 0 and year % 100 !=0) or (year % 400 == 0)
+    return calendar.isleap(year)
 
-def calculate_age(birth_year):
-    current_year = datetime.datetime.now().year
-    return current_year - birth_year
+def calculate_age(day, month, year):
+    today = datetime.date.today()
+    birth_date = datetime.date(year, month, day)
+    age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+    return age
 
-day = int(input("Введите день вашего рождения:"))
-month = int(input("введите месяц вашего рождения:"))
-year = int(input("Введите год вашего рождения:"))
+def display_date_as_stars(day, month, year):
+    day_str = f"{day:02d}"
+    month_str = f"{month:02d}"
+    year_str = f"{year:04d}"
+    date_str = f"{day_str} {month_str} {year_str}"
+    print((date_str))
 
-day_of_week_result = day_of_week(day,month,year)
-print(f"Это был день недели:{day_of_week_result}")
+def main():
+    day, month, year = get_birthday()
+    print(f"Это был день недели {day_of_week(day, month, year)}.")
+    print(f"Это был {'високосный' if is_leap_year(year) else 'не високосный'} год.")
+    print(f"Вам сейчас {calculate_age(day, month, year)} лет.")
+    print("Ваша дата рождения в формате ** ** ****:")
+    display_date_as_stars(day, month, year)
 
-leap_year_result=is_leap_year(year)
-if leap_year_result:
-    print("Это был високосный год.")
-else:
-    print("Это был не високосный год.")
-    
-age = calculate_age(year)
-print(f"Вам сейчас {age} лет.")
-
-print(f"Дата вашего рождения: {str(day).rjust(2,'*')} {str(month).rjust(2,'*')} {str(year).rjust(4,'*')}")
+if __name__ == "__main__":
+    main()
